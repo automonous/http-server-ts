85d40b95ed897776ef65ad5f4cffc1db1fc5458e
Tom O <tom.osborne@realpage.com>
2026-08-26 15:59:16 +0100

    remove node_modules and dist

--- dist/api/middleware/middlewareLogResponse.js
+++
@@ -1,9 +0,0 @@
-export async function middlewareLogResponse(_, res, next) {
-    res.on("finish", () => {
-        const statusCode = res.statusCode;
-        if (statusCode !== 200) {
-            console.log(`[NON-OK] ${_.method} ${_.url} - Status: ${statusCode}`);
-        }
-    });
-    next();
-}
--- dist/api/validate_create_user.js
+++
@@ -1,7 +0,0 @@
-import { createUser } from "../db/queries/users.js";
-import { respondWithJSON } from "./json.js";
-export async function handlerValidateNewUser(req, res) {
-    const params = req.body;
-    const createdUser = await createUser({ email: params.email });
-    respondWithJSON(res, 201, createdUser);
-}
